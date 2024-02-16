21b1a69aff5af1ec40b038e53fd53405675bfaea
mh53182 <marie.i.077145@gmail.com>
2024-02-16 11:26:57 +0900

    論理演算

--- and.py
+++ and.py
@@ -1,4 +1,4 @@
-# AもBも真なら真
+# 論理積：AもBも真なら真
 # if文で書くとこう
 
 # A, B = map(int, input().split())
@@ -8,7 +8,38 @@
 # else:
 #     print(0) 
 
-
 # and演算子を使うとこう
 a, b = map(int, input().split())
 print(a and b)
+
+
+print('#############')
+
+# 論理和：AまたはBが真なら真、も同じく
+a, b = map(int, input().split())
+print(a or b)
+
+
+print('##########################')
+
+# 否定の場合
+a = int(input())
+print(int(not a)) # intに変換しないとtrue/falseが出力される
+
+print('##########################')
+
+# 排他的論理和(XOR)の場合
+# 両方真・または両方偽の場合は偽
+a, b = map(int, input().split())
+print(a ^ b) # 演算子は"^"
+
+
+print('##########################')
+# 以下、まだメモ
+
+# NOR演算(否定的論理和）
+print(int(not(A or B)))
+# NAND演算(否定的論理積）
+print(int(not(A and B)))
+# XNOR演算(排他的否定的論理和）
+print(int(not(A ^ B)))
